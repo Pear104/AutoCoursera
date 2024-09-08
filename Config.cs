672ffc5b42404e3e7c9b3f66473b9de786496a8d
Truong <107944687+Pear104@users.noreply.github.com>
2024-09-09 06:00:47 +0700

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -2,9 +2,7 @@
 {
     internal static class Config
     {
-        public static string email { get; set; } = "truongltse182338@fpt.edu.vn";
-        public static string password { get; set; } = "T@od3obiet";
-        //public static string email { get; set; } = "abcde@fpt.edu.vn";
-        //public static string password { get; set; } = "12345678";
+        public static string email { get; set; } = "abcde@fpt.edu.vn";
+        public static string password { get; set; } = "12345678";
     }
 }
